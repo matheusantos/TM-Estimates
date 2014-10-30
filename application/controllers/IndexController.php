7550ed09d5c4afda4a94b6ca64deb722d58fe50f
Matheu <matheusantos@users.noreply.github.com>
2014-10-30 09:52:26 -0300

    controller login

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -1,18 +1,17 @@
 <?php
 
-class IndexController extends Zend_Controller_Action
-{
+class IndexController extends Zend_Controller_Action {
 
-    public function init()
-    {
+    public function init() {
         /* Initialize action controller here */
+
+        //if (!Zend_Auth::getInstance()->hasIdentity()) {
+        //    $this->_redirect('login');
+        //}
     }
 
-    public function indexAction()
-    {
+    public function indexAction() {
         // action body
     }
 
-
 }
-
--- application/controllers/LoginController.php
+++ application/controllers/LoginController.php
@@ -1,18 +1,62 @@
 <?php
 
-class LoginController extends Zend_Controller_Action
-{
+class LoginController extends Zend_Controller_Action {
 
-    public function init()
-    {
+    public function init() {
         /* Initialize action controller here */
     }
 
-    public function indexAction()
-    {
-        // action body
+    public function indexAction() {
+        /*
+        //Desabilita renderização da view
+        $this->_helper->viewRenderer->setNoRender();
+        
+        //Obter o objeto do adaptador para autenticar usando banco de dados
+        $dbAdapter = Zend_Db_Table_Abstract::getDefaultAdapter();
+        $authAdapter = new Zend_Auth_Adapter_DbTable($dbAdapter);
+
+        //Seta qual tabela e colunas procurar o usuário
+        $authAdapter->setTableName('usuario')
+                ->setIdentityColumn('login')
+                ->setCredentialColumn('senha');
+
+        //Seta as credenciais com dados vindos do formulário de login
+        $authAdapter->setIdentity($this->_getParam('login'))
+                ->setCredential($this->_getParam('senha'))
+                ->setCredentialTreatment('MD5(?)');
+
+        //Realiza autenticação
+        $result = $authAdapter->authenticate();
+
+        //Verifica se a autenticação foi válida
+        if ($result->isValid()) {
+
+            //Obtém dados do usuário
+            $usuario = $authAdapter->getResultRowObject();
+
+            //Armazena seus dados na sessão
+            $storage = Zend_Auth::getInstance()->getStorage();
+            $storage->write($usuario);
+
+            //Redireciona para o Index
+            $this->_redirect('index');
+        } else {
+            $this->_redirect('login/falha');
+        }
+
+         */
     }
 
+    public function falhaAction() {
+        
+    }
+
+    public function logoutAction() {
+        
+        //Limpa dados da Sessão
+        Zend_Auth::getInstance()->clearIdentity();
+        //Redireciona a requisição para a tela de Autenticacao novamente
+        $this->_redirect('autenticacao');
+    }
 
 }
-
