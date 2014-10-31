54393b6959587997360036c42ce48bf366e43155
Matheu <matheusantos@users.noreply.github.com>
2014-10-31 10:43:12 -0300

    conexão db

--- db/db.sql
+++ db/db.sql
@@ -2,13 +2,13 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
-USE `mydb` ;
+CREATE SCHEMA IF NOT EXISTS `Estimatech_db` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+USE `Estimatech_db` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`Pacote`
+-- Table `Estimatech_db`.`Pacote`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Pacote` (
+CREATE TABLE IF NOT EXISTS `Estimatech_db`.`Pacote` (
   `idPacote` INT NOT NULL,
   `Descricao` VARCHAR(45) NOT NULL,
   `LimiteProjetos` INT NOT NULL,
@@ -19,9 +19,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Compra`
+-- Table `Estimatech_db`.`Compra`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Compra` (
+CREATE TABLE IF NOT EXISTS `Estimatech_db`.`Compra` (
   `idCompra` INT NOT NULL,
   `DataCompra` DATE NOT NULL,
   `Validade` DATE NOT NULL,
@@ -30,16 +30,16 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Compra` (
   INDEX `fk_Compra_Pacote1_idx` (`Pacote_idPacote` ASC),
   CONSTRAINT `fk_Compra_Pacote1`
     FOREIGN KEY (`Pacote_idPacote`)
-    REFERENCES `mydb`.`Pacote` (`idPacote`)
+    REFERENCES `Estimatech_db`.`Pacote` (`idPacote`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`ClientePF`
+-- Table `Estimatech_db`.`ClientePF`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`ClientePF` (
+CREATE TABLE IF NOT EXISTS `Estimatech_db`.`ClientePF` (
   `idCPF` INT NOT NULL,
   `Nome` VARCHAR(45) NOT NULL,
   `Sobrenome` VARCHAR(45) NOT NULL,
@@ -52,9 +52,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`ClientePJ`
+-- Table `Estimatech_db`.`ClientePJ`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`ClientePJ` (
+CREATE TABLE IF NOT EXISTS `Estimatech_db`.`ClientePJ` (
   `idCNPJ` INT NOT NULL,
   `NomeFantasia` VARCHAR(45) NOT NULL,
   `Telefone` INT NOT NULL,
@@ -64,9 +64,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Cidade`
+-- Table `Estimatech_db`.`Cidade`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Cidade` (
+CREATE TABLE IF NOT EXISTS `Estimatech_db`.`Cidade` (
   `idCidade` INT NOT NULL,
   `Nome` VARCHAR(45) NOT NULL,
   `UF` VARCHAR(2) NOT NULL,
@@ -75,9 +75,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Endereco`
+-- Table `Estimatech_db`.`Endereco`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Endereco` (
+CREATE TABLE IF NOT EXISTS `Estimatech_db`.`Endereco` (
   `idEndereco` INT NOT NULL,
   `Cep` INT NOT NULL,
   `Logradouro` VARCHAR(45) NOT NULL,
@@ -89,16 +89,16 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Endereco` (
   INDEX `fk_Endereco_Cidade1_idx` (`Cidade_idCidade` ASC),
   CONSTRAINT `fk_Endereco_Cidade1`
     FOREIGN KEY (`Cidade_idCidade`)
-    REFERENCES `mydb`.`Cidade` (`idCidade`)
+    REFERENCES `Estimatech_db`.`Cidade` (`idCidade`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Cliente`
+-- Table `Estimatech_db`.`Cliente`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Cliente` (
+CREATE TABLE IF NOT EXISTS `Estimatech_db`.`Cliente` (
   `idCliente` INT NOT NULL,
   `Email` VARCHAR(45) NOT NULL,
   `Senha` VARCHAR(45) NOT NULL,
@@ -115,31 +115,31 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Cliente` (
   INDEX `fk_Cliente_Endereco1_idx` (`Endereco_idEndereco` ASC),
   CONSTRAINT `fk_Cliente_Compra`
     FOREIGN KEY (`Compra_idCompra`)
-    REFERENCES `mydb`.`Compra` (`idCompra`)
+    REFERENCES `Estimatech_db`.`Compra` (`idCompra`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Cliente_ClientePF1`
     FOREIGN KEY (`ClientePF_idCPF`)
-    REFERENCES `mydb`.`ClientePF` (`idCPF`)
+    REFERENCES `Estimatech_db`.`ClientePF` (`idCPF`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Cliente_ClientePJ1`
     FOREIGN KEY (`ClientePJ_idCNPJ`)
-    REFERENCES `mydb`.`ClientePJ` (`idCNPJ`)
+    REFERENCES `Estimatech_db`.`ClientePJ` (`idCNPJ`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Cliente_Endereco1`
     FOREIGN KEY (`Endereco_idEndereco`)
-    REFERENCES `mydb`.`Endereco` (`idEndereco`)
+    REFERENCES `Estimatech_db`.`Endereco` (`idEndereco`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Projeto`
+-- Table `Estimatech_db`.`Projeto`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Projeto` (
+CREATE TABLE IF NOT EXISTS `Estimatech_db`.`Projeto` (
   `idProjeto` INT NOT NULL,
   `Titulo` VARCHAR(45) NOT NULL,
   `DataCriacao` DATETIME NOT NULL,
@@ -153,16 +153,16 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Projeto` (
   INDEX `fk_Projeto_Cliente1_idx` (`Cliente_idCliente` ASC),
   CONSTRAINT `fk_Projeto_Cliente1`
     FOREIGN KEY (`Cliente_idCliente`)
-    REFERENCES `mydb`.`Cliente` (`idCliente`)
+    REFERENCES `Estimatech_db`.`Cliente` (`idCliente`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Recursos`
+-- Table `Estimatech_db`.`Recursos`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Recursos` (
+CREATE TABLE IF NOT EXISTS `Estimatech_db`.`Recursos` (
   `idRecursos` INT NOT NULL,
   `Descricao` VARCHAR(45) NOT NULL,
   `Carga_horaria` INT NOT NULL,
@@ -172,9 +172,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Equipe`
+-- Table `Estimatech_db`.`Equipe`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Equipe` (
+CREATE TABLE IF NOT EXISTS `Estimatech_db`.`Equipe` (
   `Recursos_idRecursos` INT NOT NULL,
   `Projeto_idProjeto` INT NOT NULL,
   `QtRecursos` INT NOT NULL,
@@ -183,12 +183,12 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Equipe` (
   INDEX `fk_Equipe_Projeto1_idx` (`Projeto_idProjeto` ASC),
   CONSTRAINT `fk_Equipe_Recursos1`
     FOREIGN KEY (`Recursos_idRecursos`)
-    REFERENCES `mydb`.`Recursos` (`idRecursos`)
+    REFERENCES `Estimatech_db`.`Recursos` (`idRecursos`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Equipe_Projeto1`
     FOREIGN KEY (`Projeto_idProjeto`)
-    REFERENCES `mydb`.`Projeto` (`idProjeto`)
+    REFERENCES `Estimatech_db`.`Projeto` (`idProjeto`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
