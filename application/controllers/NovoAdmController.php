1d4b5ad53bb8cc7f49708ff7e27eda01c22741ea
vikmais <vikmais@hotmail.com>
2014-11-02 15:35:07 -0300

    Tista administradores

---
+++ application/controllers/NovoAdmController.php
@@ -0,0 +1,22 @@
+<?php
+
+class NovoAdmController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        $this->_helper->layout->setlayout("userlayout");
+    }
+    
+    public function novoAction(){
+        $this->_helper->layout->setlayout("userlayout");
+    }
+
+
+}
+
