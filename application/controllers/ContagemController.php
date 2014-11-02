043315d263bacd7da2aceb15e4c10e2033530087
Matheu <matheusantos@users.noreply.github.com>
2014-11-02 15:16:54 -0300

    Novos Controles

---
+++ application/controllers/ContagemController.php
@@ -0,0 +1,18 @@
+<?php
+
+class ContagemController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        $this->_helper->layout->setlayout("userlayout");
+    }
+
+
+}
+
---
+++ application/controllers/DetalharController.php
@@ -0,0 +1,32 @@
+<?php
+
+class DetalharController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        $this->_helper->layout->setlayout("userlayout");
+    }
+
+    public function custosAction()
+    {
+        $this->_helper->layout->setlayout("userlayout");
+    }
+
+    public function fasesAction()
+    {
+        $this->_helper->layout->setlayout("userlayout");
+    }
+
+
+}
+
+
+
+
+
