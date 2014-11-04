2760a926f7a9ed0fe5f38051d389e991237efac0
mpaulomlata <mmalta@ymail.com>
2014-11-04 15:44:07 -0300

    Atualizacao db
    db atualizado, ( inclusão de auto preenchimento no campo data de
    criação da tabela projetos, incluído auto incremente nas chaves
    primarias de todas as tabelas).

--- db/db.sql
+++ db/db.sql
@@ -1,123 +1,106 @@
+-- MySQL Workbench Forward Engineering
+
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-CREATE SCHEMA IF NOT EXISTS `Estimatech_db` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema geros
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema Estimatech_db
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema Estimatech_db
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `Estimatech_db` DEFAULT CHARACTER SET utf8 ;
 USE `Estimatech_db` ;
-
--- -----------------------------------------------------
--- Table `Estimatech_db`.`Pacote`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Estimatech_db`.`Pacote` (
-  `idPacote` INT NOT NULL,
-  `Descricao` VARCHAR(45) NOT NULL,
-  `LimiteProjetos` INT NOT NULL,
-  `Validate` DATETIME NOT NULL,
-  `Valor` FLOAT NOT NULL,
-  PRIMARY KEY (`idPacote`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `Estimatech_db`.`Compra`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Estimatech_db`.`Compra` (
-  `idCompra` INT NOT NULL,
-  `DataCompra` DATE NOT NULL,
-  `Validade` DATE NOT NULL,
-  `Pacote_idPacote` INT NOT NULL,
-  PRIMARY KEY (`idCompra`),
-  INDEX `fk_Compra_Pacote1_idx` (`Pacote_idPacote` ASC),
-  CONSTRAINT `fk_Compra_Pacote1`
-    FOREIGN KEY (`Pacote_idPacote`)
-    REFERENCES `Estimatech_db`.`Pacote` (`idPacote`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
 
 -- -----------------------------------------------------
 -- Table `Estimatech_db`.`ClientePF`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Estimatech_db`.`ClientePF` (
-  `idCPF` INT NOT NULL,
+  `idCPF` INT(11) NOT NULL AUTO_INCREMENT,
   `Nome` VARCHAR(45) NOT NULL,
   `Sobrenome` VARCHAR(45) NOT NULL,
-  `Telefone` INT NOT NULL,
+  `Telefone` INT(11) NOT NULL,
   `datNasc` DATE NOT NULL,
-  `Sexo` CHAR NOT NULL,
+  `Sexo` CHAR(1) NOT NULL,
   `dataCadastro` DATETIME NOT NULL,
   PRIMARY KEY (`idCPF`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
 -- Table `Estimatech_db`.`ClientePJ`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Estimatech_db`.`ClientePJ` (
-  `idCNPJ` INT NOT NULL,
+  `idCNPJ` INT(11) NOT NULL AUTO_INCREMENT,
   `NomeFantasia` VARCHAR(45) NOT NULL,
-  `Telefone` INT NOT NULL,
+  `Telefone` INT(11) NOT NULL,
   `dataCadastro` DATETIME NOT NULL,
   PRIMARY KEY (`idCNPJ`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `Estimatech_db`.`Cidade`
+-- Table `Estimatech_db`.`Pacote`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Estimatech_db`.`Cidade` (
-  `idCidade` INT NOT NULL,
-  `Nome` VARCHAR(45) NOT NULL,
-  `UF` VARCHAR(2) NOT NULL,
-  PRIMARY KEY (`idCidade`))
-ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS `Estimatech_db`.`Pacote` (
+  `idPacote` INT(11) NOT NULL AUTO_INCREMENT,
+  `Descricao` VARCHAR(45) NOT NULL,
+  `LimiteProjetos` INT(11) NOT NULL,
+  `Validate` DATETIME NOT NULL,
+  `Valor` FLOAT NOT NULL,
+  PRIMARY KEY (`idPacote`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `Estimatech_db`.`Endereco`
+-- Table `Estimatech_db`.`Compra`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Estimatech_db`.`Endereco` (
-  `idEndereco` INT NOT NULL,
-  `Cep` INT NOT NULL,
-  `Logradouro` VARCHAR(45) NOT NULL,
-  `Bairro` VARCHAR(45) NOT NULL,
-  `Numero` INT NOT NULL,
-  `Complemento` VARCHAR(45) NULL,
-  `Cidade_idCidade` INT NOT NULL,
-  PRIMARY KEY (`idEndereco`),
-  INDEX `fk_Endereco_Cidade1_idx` (`Cidade_idCidade` ASC),
-  CONSTRAINT `fk_Endereco_Cidade1`
-    FOREIGN KEY (`Cidade_idCidade`)
-    REFERENCES `Estimatech_db`.`Cidade` (`idCidade`)
+CREATE TABLE IF NOT EXISTS `Estimatech_db`.`Compra` (
+  `idCompra` INT(11) NOT NULL AUTO_INCREMENT,
+  `DataCompra` DATE NOT NULL,
+  `Validade` DATE NOT NULL,
+  `Pacote_idPacote` INT(11) NOT NULL,
+  PRIMARY KEY (`idCompra`),
+  CONSTRAINT `fk_Compra_Pacote1`
+    FOREIGN KEY (`Pacote_idPacote`)
+    REFERENCES `Estimatech_db`.`Pacote` (`idPacote`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
 -- Table `Estimatech_db`.`Cliente`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Estimatech_db`.`Cliente` (
-  `idCliente` INT NOT NULL,
+  `idCliente` INT(11) NOT NULL AUTO_INCREMENT,
   `Email` VARCHAR(45) NOT NULL,
   `Senha` VARCHAR(45) NOT NULL,
-  `Endereco_idEndereco` INT NOT NULL,
-  `Compra_idCompra` INT NOT NULL,
-  `ClientePF_idCPF` INT NULL,
-  `ClientePJ_idCNPJ` INT NULL,
+  `Compra_idCompra` INT(11) NOT NULL,
+  `ClientePF_idCPF` INT(11) NULL DEFAULT NULL,
+  `ClientePJ_idCNPJ` INT(11) NULL DEFAULT NULL,
   `EXCLUIDO` TINYINT(1) NOT NULL,
+  `Cep` INT(11) NULL DEFAULT NULL,
+  `Logradouro` VARCHAR(45) NULL DEFAULT NULL,
+  `Bairro` VARCHAR(45) NULL DEFAULT NULL,
+  `Numero` INT(11) NULL DEFAULT NULL,
+  `Complemento` VARCHAR(45) NULL DEFAULT NULL,
+  `Cidade` VARCHAR(45) NULL DEFAULT NULL,
+  `UF` VARCHAR(2) NULL DEFAULT NULL,
   PRIMARY KEY (`idCliente`),
   UNIQUE INDEX `email_UNIQUE` (`Email` ASC),
-  INDEX `fk_Cliente_Compra_idx` (`Compra_idCompra` ASC),
-  INDEX `fk_Cliente_ClientePF1_idx` (`ClientePF_idCPF` ASC),
-  INDEX `fk_Cliente_ClientePJ1_idx` (`ClientePJ_idCNPJ` ASC),
-  INDEX `fk_Cliente_Endereco1_idx` (`Endereco_idEndereco` ASC),
-  CONSTRAINT `fk_Cliente_Compra`
-    FOREIGN KEY (`Compra_idCompra`)
-    REFERENCES `Estimatech_db`.`Compra` (`idCompra`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
   CONSTRAINT `fk_Cliente_ClientePF1`
     FOREIGN KEY (`ClientePF_idCPF`)
     REFERENCES `Estimatech_db`.`ClientePF` (`idCPF`)
@@ -128,70 +111,72 @@ CREATE TABLE IF NOT EXISTS `Estimatech_db`.`Cliente` (
     REFERENCES `Estimatech_db`.`ClientePJ` (`idCNPJ`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Cliente_Endereco1`
-    FOREIGN KEY (`Endereco_idEndereco`)
-    REFERENCES `Estimatech_db`.`Endereco` (`idEndereco`)
+  CONSTRAINT `fk_Cliente_Compra`
+    FOREIGN KEY (`Compra_idCompra`)
+    REFERENCES `Estimatech_db`.`Compra` (`idCompra`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
 -- Table `Estimatech_db`.`Projeto`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Estimatech_db`.`Projeto` (
-  `idProjeto` INT NOT NULL,
+  `idProjeto` INT(11) NOT NULL AUTO_INCREMENT,
   `Titulo` VARCHAR(45) NOT NULL,
-  `DataCriacao` DATETIME NOT NULL,
+  `DataCriacao` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `DataFininalizacao` DATETIME NOT NULL,
   `Categoria` VARCHAR(45) NOT NULL,
-  `Status` VARCHAR(45) NULL,
-  `GerenciaProjeto_Cliente_idCliente` INT NOT NULL,
+  `Status` VARCHAR(45) NULL DEFAULT NULL,
+  `GerenciaProjeto_Cliente_idCliente` INT(11) NOT NULL,
   `UltimaAtualizacao` DATETIME NOT NULL,
-  `Cliente_idCliente` INT NOT NULL,
+  `Cliente_idCliente` INT(11) NOT NULL,
   PRIMARY KEY (`idProjeto`),
-  INDEX `fk_Projeto_Cliente1_idx` (`Cliente_idCliente` ASC),
   CONSTRAINT `fk_Projeto_Cliente1`
     FOREIGN KEY (`Cliente_idCliente`)
     REFERENCES `Estimatech_db`.`Cliente` (`idCliente`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
 -- Table `Estimatech_db`.`Recursos`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Estimatech_db`.`Recursos` (
-  `idRecursos` INT NOT NULL,
+  `idRecursos` INT(11) NOT NULL,
   `Descricao` VARCHAR(45) NOT NULL,
-  `Carga_horaria` INT NOT NULL,
+  `Carga_horaria` INT(11) NOT NULL,
   `Custo` DECIMAL(3,2) NOT NULL,
   PRIMARY KEY (`idRecursos`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
 -- Table `Estimatech_db`.`Equipe`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Estimatech_db`.`Equipe` (
-  `Recursos_idRecursos` INT NOT NULL,
-  `Projeto_idProjeto` INT NOT NULL,
-  `QtRecursos` INT NOT NULL,
+  `Recursos_idRecursos` INT(11) NOT NULL AUTO_INCREMENT,
+  `Projeto_idProjeto` INT(11) NOT NULL,
+  `QtRecursos` INT(11) NOT NULL,
   PRIMARY KEY (`Recursos_idRecursos`, `Projeto_idProjeto`),
   INDEX `fk_Equipe_Recursos1_idx` (`Recursos_idRecursos` ASC),
-  INDEX `fk_Equipe_Projeto1_idx` (`Projeto_idProjeto` ASC),
-  CONSTRAINT `fk_Equipe_Recursos1`
-    FOREIGN KEY (`Recursos_idRecursos`)
-    REFERENCES `Estimatech_db`.`Recursos` (`idRecursos`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
   CONSTRAINT `fk_Equipe_Projeto1`
     FOREIGN KEY (`Projeto_idProjeto`)
     REFERENCES `Estimatech_db`.`Projeto` (`idProjeto`)
     ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Equipe_Recursos1`
+    FOREIGN KEY (`Recursos_idRecursos`)
+    REFERENCES `Estimatech_db`.`Recursos` (`idRecursos`)
+    ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
