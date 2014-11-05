8dbb3a69833f301a6fce6c825195be47d9e04721
Matheu <matheusantos@users.noreply.github.com>
2014-11-05 14:48:22 -0300

    operação de insert e select em recursos

--- application/controllers/RecursoController.php
+++ application/controllers/RecursoController.php
@@ -1,27 +1,36 @@
 <?php
 
-class RecursoController extends Zend_Controller_Action
-{
+class RecursoController extends Zend_Controller_Action {
 
-    public function init()
-    {
+    public function init() {
         /* Initialize action controller here */
     }
 
-    public function indexAction()
-    {
+    public function indexAction() {
+        // action body
+        $this->_helper->layout->setlayout("userlayout");
+
+        $model = new Application_Model_Recursos();
+        $dados = $model->_select();
+        $this->view->assign("dados", $dados);
+    }
+
+    public function showAction() {
+        $model = new Application_Model_Recursos();
+        $recursos = $model->find($this->_getParam('id'));
+        $this->view->assign("recursos", $recursos);
+    }
+
+    public function novoAction() {
         // action body
         $this->_helper->layout->setlayout("userlayout");
     }
 
-    public function novoAction()
-    {
-        // action body
-        $this->_helper->layout->setlayout("userlayout");
+    public function salvarDadosAction() {
+        $dados = $this->_getAllParams();
+        $model = new Application_Model_Recursos();
+        $model->inserir($dados);
+        $this->_redirect("/recurso");
     }
-
 
 }
-
-
-
--- db/db.sql
+++ db/db.sql
@@ -1,29 +1,14 @@
--- MySQL Workbench Forward Engineering
-
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
--- Schema geros
--- -----------------------------------------------------
--- -----------------------------------------------------
--- Schema Estimatech_db
--- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `estimatech_db` DEFAULT CHARACTER SET utf8 ;
+USE `estimatech_db` ;
 
 -- -----------------------------------------------------
--- Schema Estimatech_db
+-- Table `estimatech_db`.`clientepf`
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `Estimatech_db` DEFAULT CHARACTER SET utf8 ;
-USE `Estimatech_db` ;
-
--- -----------------------------------------------------
--- Table `Estimatech_db`.`ClientePF`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Estimatech_db`.`ClientePF` (
+CREATE TABLE IF NOT EXISTS `estimatech_db`.`clientepf` (
   `idCPF` INT(11) NOT NULL AUTO_INCREMENT,
   `Nome` VARCHAR(45) NOT NULL,
   `Sobrenome` VARCHAR(45) NOT NULL,
@@ -37,9 +22,9 @@ DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `Estimatech_db`.`ClientePJ`
+-- Table `estimatech_db`.`clientepj`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Estimatech_db`.`ClientePJ` (
+CREATE TABLE IF NOT EXISTS `estimatech_db`.`clientepj` (
   `idCNPJ` INT(11) NOT NULL AUTO_INCREMENT,
   `NomeFantasia` VARCHAR(45) NOT NULL,
   `Telefone` INT(11) NOT NULL,
@@ -50,9 +35,9 @@ DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `Estimatech_db`.`Pacote`
+-- Table `estimatech_db`.`pacote`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Estimatech_db`.`Pacote` (
+CREATE TABLE IF NOT EXISTS `estimatech_db`.`pacote` (
   `idPacote` INT(11) NOT NULL AUTO_INCREMENT,
   `Descricao` VARCHAR(45) NOT NULL,
   `LimiteProjetos` INT(11) NOT NULL,
@@ -64,17 +49,18 @@ DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `Estimatech_db`.`Compra`
+-- Table `estimatech_db`.`compra`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Estimatech_db`.`Compra` (
+CREATE TABLE IF NOT EXISTS `estimatech_db`.`compra` (
   `idCompra` INT(11) NOT NULL AUTO_INCREMENT,
   `DataCompra` DATE NOT NULL,
   `Validade` DATE NOT NULL,
   `Pacote_idPacote` INT(11) NOT NULL,
   PRIMARY KEY (`idCompra`),
+  INDEX `fk_Compra_Pacote1` (`Pacote_idPacote` ASC),
   CONSTRAINT `fk_Compra_Pacote1`
     FOREIGN KEY (`Pacote_idPacote`)
-    REFERENCES `Estimatech_db`.`Pacote` (`idPacote`)
+    REFERENCES `estimatech_db`.`pacote` (`idPacote`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
@@ -82,9 +68,9 @@ DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `Estimatech_db`.`Cliente`
+-- Table `estimatech_db`.`cliente`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Estimatech_db`.`Cliente` (
+CREATE TABLE IF NOT EXISTS `estimatech_db`.`cliente` (
   `idCliente` INT(11) NOT NULL AUTO_INCREMENT,
   `Email` VARCHAR(45) NOT NULL,
   `Senha` VARCHAR(45) NOT NULL,
@@ -101,19 +87,22 @@ CREATE TABLE IF NOT EXISTS `Estimatech_db`.`Cliente` (
   `UF` VARCHAR(2) NULL DEFAULT NULL,
   PRIMARY KEY (`idCliente`),
   UNIQUE INDEX `email_UNIQUE` (`Email` ASC),
+  INDEX `fk_Cliente_ClientePF1` (`ClientePF_idCPF` ASC),
+  INDEX `fk_Cliente_ClientePJ1` (`ClientePJ_idCNPJ` ASC),
+  INDEX `fk_Cliente_Compra` (`Compra_idCompra` ASC),
   CONSTRAINT `fk_Cliente_ClientePF1`
     FOREIGN KEY (`ClientePF_idCPF`)
-    REFERENCES `Estimatech_db`.`ClientePF` (`idCPF`)
+    REFERENCES `estimatech_db`.`clientepf` (`idCPF`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Cliente_ClientePJ1`
     FOREIGN KEY (`ClientePJ_idCNPJ`)
-    REFERENCES `Estimatech_db`.`ClientePJ` (`idCNPJ`)
+    REFERENCES `estimatech_db`.`clientepj` (`idCNPJ`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Cliente_Compra`
     FOREIGN KEY (`Compra_idCompra`)
-    REFERENCES `Estimatech_db`.`Compra` (`idCompra`)
+    REFERENCES `estimatech_db`.`compra` (`idCompra`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
@@ -121,58 +110,57 @@ DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `Estimatech_db`.`Projeto`
+-- Table `estimatech_db`.`projeto`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Estimatech_db`.`Projeto` (
+CREATE TABLE IF NOT EXISTS `estimatech_db`.`projeto` (
   `idProjeto` INT(11) NOT NULL AUTO_INCREMENT,
   `Titulo` VARCHAR(45) NOT NULL,
   `DataCriacao` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `DataFininalizacao` DATETIME NOT NULL,
   `Categoria` VARCHAR(45) NOT NULL,
-  `Status` VARCHAR(45) NULL DEFAULT NULL,
-  `GerenciaProjeto_Cliente_idCliente` INT(11) NOT NULL,
+  `Situacao` CHAR(1) NOT NULL,
   `UltimaAtualizacao` DATETIME NOT NULL,
   `Cliente_idCliente` INT(11) NOT NULL,
   PRIMARY KEY (`idProjeto`),
+  INDEX `fk_Projeto_Cliente1` (`Cliente_idCliente` ASC),
   CONSTRAINT `fk_Projeto_Cliente1`
     FOREIGN KEY (`Cliente_idCliente`)
-    REFERENCES `Estimatech_db`.`Cliente` (`idCliente`)
+    REFERENCES `estimatech_db`.`cliente` (`idCliente`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Estimatech_db`.`Recursos`
+-- Table `estimatech_db`.`recursos`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Estimatech_db`.`Recursos` (
-  `idRecursos` INT(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `estimatech_db`.`recursos` (
+  `idRecursos` INT(11) NOT NULL AUTO_INCREMENT,
   `Descricao` VARCHAR(45) NOT NULL,
   `Carga_horaria` INT(11) NOT NULL,
   `Custo` DECIMAL(3,2) NOT NULL,
   PRIMARY KEY (`idRecursos`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Estimatech_db`.`Equipe`
+-- Table `estimatech_db`.`equipe`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Estimatech_db`.`Equipe` (
+CREATE TABLE IF NOT EXISTS `estimatech_db`.`equipe` (
   `Recursos_idRecursos` INT(11) NOT NULL AUTO_INCREMENT,
   `Projeto_idProjeto` INT(11) NOT NULL,
   `QtRecursos` INT(11) NOT NULL,
   PRIMARY KEY (`Recursos_idRecursos`, `Projeto_idProjeto`),
   INDEX `fk_Equipe_Recursos1_idx` (`Recursos_idRecursos` ASC),
+  INDEX `fk_Equipe_Projeto1` (`Projeto_idProjeto` ASC),
   CONSTRAINT `fk_Equipe_Projeto1`
     FOREIGN KEY (`Projeto_idProjeto`)
-    REFERENCES `Estimatech_db`.`Projeto` (`idProjeto`)
+    REFERENCES `estimatech_db`.`projeto` (`idProjeto`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Equipe_Recursos1`
     FOREIGN KEY (`Recursos_idRecursos`)
-    REFERENCES `Estimatech_db`.`Recursos` (`idRecursos`)
+    REFERENCES `estimatech_db`.`recursos` (`idRecursos`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
