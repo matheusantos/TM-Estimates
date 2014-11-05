1fb42950fa20ca5ed53f9d24bf5c6dd0cc826199
Matheu <matheusantos@users.noreply.github.com>
2014-11-05 16:02:16 -0300

    model clientepf

---
+++ application/models/ClientePF.php
@@ -0,0 +1,54 @@
+<?php
+
+class Application_Model_ClientePF extends Zend_Db_Table_Abstract {
+
+    protected $_name = "clientepf";
+    protected $_primary = "idCPF";
+
+    public function inserir(array $request) {
+        $data = date('Y-m-d H:i:s');
+        
+        $dados = array(
+            'Nome' => $request['nome'],
+            'Sobrenome' => $request['sobrenome'],
+            'Telefone' => $request['custo'],
+            'datNasc' => $request['datNasc'],
+            'Sexo' => $request['sexo'],
+            'dataCadastro' => $data
+        );
+        return $this->insert($dados);
+    }
+
+    public function _select($where = null, $order = null, $limit = null) {
+        $select = $this->select()
+                        ->from($this)
+                        ->order($order)
+                        ->limit($limit);
+		
+        if (!is_null($where)) {
+            $select->where($where);
+        }
+        return $this->fetchAll($select)->toArray();
+    }
+
+    public function find($id) {
+        $arr = $this->find($id)->toArray();
+        return $arr[0];
+    }
+
+    public function update(array $request) {
+        $dados = array(
+            'Descricao' => $request['descricao'],
+            'Carga_horaria' => $request['carga_horaria'],
+            'Custo' => $request['custo']
+        );
+        $where = $this->getAdapter()->quoteInto("contato_id = ?", $request['contato_id']);
+        $this->update($dados, $where);
+    }
+
+    public function delete($id) {
+        $where = $this->getAdapter()->quoteInto("contato_id = ?", $id);
+        $this->delete($where);
+    }
+
+}
---
+++ application/models/ClientePJ.php
@@ -0,0 +1,49 @@
+<?php
+
+class Application_Model_Recursos extends Zend_Db_Table_Abstract {
+
+    protected $_name = "recursos";
+    protected $_primary = "idRecursos";
+
+    public function inserir(array $request) {
+        $dados = array(
+            'Descricao' => $request['descricao'],
+            'Carga_horaria' => $request['carga_horaria'],
+            'Custo' => $request['custo'],
+        );
+        return $this->insert($dados);
+    }
+
+    public function _select($where = null, $order = null, $limit = null) {
+        $select = $this->select()
+				->from($this)
+				->order($order)
+				->limit($limit);
+		
+        if (!is_null($where)) {
+            $select->where($where);
+        }
+        return $this->fetchAll($select)->toArray();
+    }
+
+    public function find($id) {
+        $arr = $this->find($id)->toArray();
+        return $arr[0];
+    }
+
+    public function update(array $request) {
+        $dados = array(
+            'Descricao' => $request['descricao'],
+            'Carga_horaria' => $request['carga_horaria'],
+            'Custo' => $request['custo']
+        );
+        $where = $this->getAdapter()->quoteInto("contato_id = ?", $request['contato_id']);
+        $this->update($dados, $where);
+    }
+
+    public function delete($id) {
+        $where = $this->getAdapter()->quoteInto("contato_id = ?", $id);
+        $this->delete($where);
+    }
+
+}
