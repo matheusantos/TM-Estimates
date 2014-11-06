cfa83a4d7330f3779f9124fb9df8e5e32e430ea6
José Barreto dos Santos Neto <jbsneto18@gmail.com>
2014-11-06 01:03:55 -0200

    DB Arquivo Fases

--- application/controllers/FaseController.php
+++ application/controllers/FaseController.php
@@ -1,21 +1,34 @@
 <?php
 
-class FaseController extends Zend_Controller_Action
-{
+class FaseController extends Zend_Controller_Action {
 
-    public function init()
-    {
+    public function init() {
         /* Initialize action controller here */
     }
 
-    public function indexAction()
-    {
+    public function indexAction() {
         $this->_helper->layout->setlayout("userlayout");
+
+        $model = new Application_Model_Fase();
+        $dados = $model->_select();
+        $this->view->assign("dados", $dados);
+    }
+    
+        public function showAction() {
+        $model = new Application_Model_Fase();
+        $fase = $model->find($this->_getParam('id'));
+        $this->view->assign("fase", $fase);
     }
 
-    public function novoAction()
-    {
+    public function novoAction() {
         $this->_helper->layout->setlayout("userlayout");
     }
+    
+        public function salvarDadosAction() {
+        $dados = $this->_getAllParams();
+        $model = new Application_Model_Fase();
+        $model->inserir($dados);
+        $this->_redirect("/fase");
+    }
+
 }
-
---
+++ application/models/Fase.php
@@ -0,0 +1,46 @@
+<?php
+
+class Application_Model_Fase extends Zend_Db_Table_Abstract {
+
+    protected $_name = "fase";
+    protected $_primary = "idFase";
+
+    public function inserir(array $request) {
+        $dados = array(
+            'Descricao' => $request['Desc'],
+            'Cliente_idCliente' => "1"
+        );
+        return $this->insert($dados);
+    }
+
+    public function _select($where = null, $order = null, $limit = null) {
+        $select = $this->select()
+                ->from($this)
+                ->order($order)
+                ->limit($limit);
+
+        if (!is_null($where)) {
+            $select->where($where);
+        }
+        return $this->fetchAll($select)->toArray();
+    }
+
+    public function find($id) {
+        $arr = $this->find($id)->toArray();
+        return $arr[0];
+    }
+
+    public function update(array $request) {
+        $dados = array(
+            'Descricao' => $request['Desc'],
+        );
+        $where = $this->getAdapter()->quoteInto("contato_id = ?", $request['contato_id']);
+        $this->update($dados, $where);
+    }
+
+    public function delete($id) {
+        $where = $this->getAdapter()->quoteInto("contato_id = ?", $id);
+        $this->delete($where);
+    }
+
+}
