702c4f9b2fe902186eafe663025da5714af730b5
José Barreto dos Santos Neto <jbsneto18@gmail.com>
2014-11-06 15:47:44 -0200

    Db Equipe

--- application/controllers/EquipeController.php
+++ application/controllers/EquipeController.php
@@ -1,37 +1,44 @@
 <?php
 
-class EquipeController extends Zend_Controller_Action
-{
+class EquipeController extends Zend_Controller_Action {
 
-    public function init()
-    {
+    public function init() {
         /* Initialize action controller here */
-        
-        
     }
 
-    public function indexAction()
-    {
+    public function indexAction() {
         // action body
         $this->_helper->layout->setlayout("userlayout");
+
+        $model = new Application_Model_Equipe();
+        $dados = $model->_select();
+        $this->view->assign("dados", $dados);
     }
 
-    public function novoAction()
-    {
+    public function showAction() {
+        $model = new Application_Model_Equipe();
+        $equipe = $model->find($this->_getParam('id'));
+        $this->view->assign("equipe", $equipe);
+    }
+
+    public function novoAction() {
         // action body
         $this->_helper->layout->setlayout("userlayout");
-        
+
         $model = new Application_Model_Recursos();
         $dados = $model->_select();
         $this->view->assign("dados", $dados);
-        
-         $model1 = new Application_Model_Projeto();
+
+        $model1 = new Application_Model_Projeto();
         $dados1 = $model1->db_select();
         $this->view->assign("dados1", $dados1);
     }
 
+    public function salvarDadosAction() {
+        $dados = $this->_getAllParams();
+        $model = new Application_Model_Equipe();
+        $model->inserir($dados);
+        $this->_redirect("/equipe");
+    }
 
 }
-
-
-
