7c0b94f4def0b953609023a595adfa0b308f9ba1
Matheu <matheusantos@users.noreply.github.com>
2014-11-06 17:37:51 -0300

    db equipe

--- application/controllers/EquipeController.php
+++ application/controllers/EquipeController.php
@@ -2,37 +2,36 @@
 
 class EquipeController extends Zend_Controller_Action {
 
+    var $usuario;
+
     public function init() {
-        /* Initialize action controller here */
+        if (!Zend_Auth::getInstance()->hasIdentity()) {
+            $this->_redirect('/login');
+        }
+
+        $auth = Zend_Auth::getInstance();
+        if ($auth->hasIdentity()) {
+            $identity = $auth->getIdentity();
+            $this->usuario = get_object_vars($identity);
+        }
+
+        $this->_helper->layout->setlayout("userlayout");
     }
 
     public function indexAction() {
-        // action body
-        $this->_helper->layout->setlayout("userlayout");
-
-        $model = new Application_Model_Equipe();
-        $dados = $model->_select();
-        $this->view->assign("dados", $dados);
+        $db = Zend_Db_Table_Abstract::getDefaultAdapter();
+                
+        $var = $db->select()
+                ->from('projeto', array())
+                ->from('equipe')
+                ->where('Cliente_idCLiente = ' . $this->usuario['idCliente'])
+                ->where('idProjeto = Projeto_idProjeto');
+        
+        $dados = $db->fetchAll($var);
+        $this->view->assign("dados", $dados); 
     }
 
-    public function showAction() {
-        $model = new Application_Model_Equipe();
-        $equipe = $model->find($this->_getParam('id'));
-        $this->view->assign("equipe", $equipe);
-    }
-
-    public function novoAction() {
-        // action body
-        $this->_helper->layout->setlayout("userlayout");
-
-        $model = new Application_Model_Recursos();
-        $dados = $model->_select();
-        $this->view->assign("dados", $dados);
-
-        $model1 = new Application_Model_Projeto();
-        $dados1 = $model1->db_select();
-        $this->view->assign("dados1", $dados1);
-    }
+    public function novoAction() { }
 
     public function salvarDadosAction() {
         $dados = $this->_getAllParams();
