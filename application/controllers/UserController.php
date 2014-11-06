1c80b9d4e216cab001b08989423820a83e3f1276
Matheu <matheusantos@users.noreply.github.com>
2014-11-06 17:47:59 -0300

    modificações pagina depois de login

--- application/controllers/UserController.php
+++ application/controllers/UserController.php
@@ -8,7 +8,10 @@ class UserController extends Zend_Controller_Action {
         if (!Zend_Auth::getInstance()->hasIdentity()) {
             $this->_redirect('/login');
         }
-        $this->_helper->layout->setlayout("userlayout");
+//        $this->_helper->layout->setlayout("userlayout");
+        
+        $this->_redirect("/projeto");
+        
     }
 
     public function indexAction() {
