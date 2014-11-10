bd4ca20c626fd2b28fb2b22342a1f6c5b79f40b9
José Barreto dos Santos Neto <jbsneto18@gmail.com>
2014-11-10 14:23:58 -0200

    validarSenha

--- application/controllers/CadastroUserController.php
+++ application/controllers/CadastroUserController.php
@@ -1,48 +1,53 @@
 <?php
-/*! Controler Cadastro de Usuario */
-class CadastroUserController extends Zend_Controller_Action
-{
 
-    public function init()
-    {
+/* ! Controler Cadastro de Usuario */
+
+class CadastroUserController extends Zend_Controller_Action {
+
+    public function init() {
         /* Initialize action controller here */
     }
 
-    public function indexAction()
-    {
+    public function indexAction() {
         // action body
     }
 
-    public function pjAction()
-    {
+    public function pjAction() {
         // action body
     }
-    
-    public function cadastrarAction()
-    {
-        $dados = $this->_getAllParams();
-        
-        $cliente = new Application_Model_Cliente();
-        
-        $email = $cliente->db_select('Email', $dados['email']);
 
-        if (empty($email))
-            $this->_redirect("/cadastro-user");
-            
-        if ($dados['email'] == $email[0]["Email"]) {
-            echo "Arrumar um jeito de retorna Email ja cadastrado no form";
-            die;
+    public function cadastrarAction() {
+        $senha = $_POST["senha"];
+        $senha2 = $_POST["ConfSenha"];
+
+        if ($senha != $senha2) {
+            echo "Já temos essa assinatura!";
+        } else {
+            $dados = $this->_getAllParams();
+
+            $cliente = new Application_Model_Cliente();
+
+            $email = $cliente->db_select('Email', $dados['email']);
+
+            if (empty($email))
+                $this->_redirect("/cadastro-user");
+
+            if ($dados['email'] == $email[0]["Email"]) {
+                echo "Arrumar um jeito de retorna Email ja cadastrado no form";
+                die;
+            }
+
+            $radio = $dados['radio_box'];
+            if ($radio == "pf")
+                $model = new Application_Model_ClientePF();
+            else
+                $model = new Application_Model_ClientePJ();
+
+            $id = $cliente->inserir($dados);
+            $model->inserir($dados, $id);
+
+            $this->_redirect("/login");
         }
-        
-        $radio = $dados['radio_box'];
-        if ($radio == "pf")
-            $model = new Application_Model_ClientePF();
-        else
-            $model = new Application_Model_ClientePJ();
-        
-        $id = $cliente->inserir($dados);        
-        $model->inserir($dados, $id);
-        
-        $this->_redirect("/login");
     }
+
 }
