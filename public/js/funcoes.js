fd9bef80322253a8332f6f139fa6267358b9bcdd
José Barreto dos Santos Neto <jbsneto18@gmail.com>
2014-11-10 15:17:41 -0200

    ValidarSenha(1)

--- public/js/funcoes.js
+++ public/js/funcoes.js
@@ -5,46 +5,56 @@
  */
 
 
-function setPJ(){
-    location.href="http://localhost/TM-Estimates/public/cadastro-user/pj";
+function setPJ() {
+    location.href = "http://localhost/TM-Estimates/public/cadastro-user/pj";
 }
 
-function setPF(){
-    location.href="http://localhost/TM-Estimates/public/cadastro-user/index";
+function setPF() {
+    location.href = "http://localhost/TM-Estimates/public/cadastro-user/index";
 }
 
-function setVisaE(){
-    location.href="http://localhost/TM-Estimates/public/vendas/pagamento-debito";
+function setVisaE() {
+    location.href = "http://localhost/TM-Estimates/public/vendas/pagamento-debito";
 }
 
-function setVisa(){
-    location.href="http://localhost/TM-Estimates/public/vendas/pagamento";
+function setVisa() {
+    location.href = "http://localhost/TM-Estimates/public/vendas/pagamento";
 }
 
-function setBoleto(){
-    location.href="http://localhost/TM-Estimates/public/vendas/pagamento-boleto";
+function setBoleto() {
+    location.href = "http://localhost/TM-Estimates/public/vendas/pagamento-boleto";
 }
 
-function setMaster(){
-    location.href="http://localhost/TM-Estimates/public/vendas/pagamento-master";
+function setMaster() {
+    location.href = "http://localhost/TM-Estimates/public/vendas/pagamento-master";
 }
 
-function setHiper(){
-    location.href="http://localhost/TM-Estimates/public/vendas/pagamento-hiper";
+function setHiper() {
+    location.href = "http://localhost/TM-Estimates/public/vendas/pagamento-hiper";
 }
 
-function setAmerica(){
-    location.href="http://localhost/TM-Estimates/public/vendas/pagamento-america";
+function setAmerica() {
+    location.href = "http://localhost/TM-Estimates/public/vendas/pagamento-america";
 }
 
-function setPag(){
-    location.href="http://localhost/TM-Estimates/public/vendas/pagamento";
+function setPag() {
+    location.href = "http://localhost/TM-Estimates/public/vendas/pagamento";
 }
 
-function setCancel(){
-    location.href="http://localhost/TM-Estimates/public/vendas";
+function setCancel() {
+    location.href = "http://localhost/TM-Estimates/public/vendas";
 }
 
-function setIni(){
-    location.href="http://localhost/TM-Estimates/public";
+function setIni() {
+    location.href = "http://localhost/TM-Estimates/public";
+}
+
+function validarSenha() {
+    var senha = cadastro.senha.value;
+    var rep_senha = cadastro.ConfSenha.value;
+    
+    if(senha != rep_senha){
+        alert("As senhas não conferem!");
+        document.bgColor='red';
+    }
 }
