382abca4edfdf7e2edf08cedfbed9ca517257f91
José Barreto dos Santos Neto <jbsneto18@gmail.com>
2014-11-12 19:16:24 -0300

    Editar incompleto

--- application/controllers/EditarController.php
+++ application/controllers/EditarController.php
@@ -18,6 +18,50 @@ class EditarController extends Zend_Controller_Action
         
                 
     }
+    
+        public function equipeAction() {
+        $id = $this->getParam('idP');
+        
+        $model_cliente = new Application_Model_Equipe();
+        $dados_cliente = $model_cliente->db_select(null, $id, null, null);
+        $this->view->assign("id", $id);
+        $this->view->assign("dados_cliente", $dados_cliente);
+        
+                
+    }
+    
+        public function faseAction() {
+        $id = $this->getParam('idP');
+        
+        $model_cliente = new Application_Model_Fase();
+        $dados_cliente = $model_cliente->db_select(null, $id, null, null);
+        $this->view->assign("id", $id);
+        $this->view->assign("dados_cliente", $dados_cliente);
+        
+                
+    }
+    
+        public function ambienteAction() {
+        $id = $this->getParam('idP');
+        
+        $model_cliente = new Application_Model_Ambiente();
+        $dados_cliente = $model_cliente->db_select(null, $id, null, null);
+        $this->view->assign("id", $id);
+        $this->view->assign("dados_cliente", $dados_cliente);
+        
+                
+    }
+    
+            public function recursoAction() {
+        $id = $this->getParam('idP');
+        
+        $model_cliente = new Application_Model_Recursos();
+        $dados_cliente = $model_cliente->db_select(null, $id, null, null);
+        $this->view->assign("id", $id);
+        $this->view->assign("dados_cliente", $dados_cliente);
+        
+                
+    }
    
 }
 
