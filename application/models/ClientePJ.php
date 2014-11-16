fb269021be0b15ce4595272496e4776feb8fa80f
Matheu <matheusantos@users.noreply.github.com>
2014-11-16 12:15:32 -0300

    correção cadastro pj e validar senha

--- application/models/ClientePJ.php
+++ application/models/ClientePJ.php
@@ -6,12 +6,11 @@ class Application_Model_ClientePJ extends Zend_Db_Table_Abstract {
     protected $_primary = "idCNPJ";
 
     public function inserir(array $request, $id) {
-        $data = date('Y-m-d H:i:s');
+        //$data = date('Y-m-d H:i:s');
         
         $dados = array(
             'NomeFantasia' => $request['nomeFant'],
             'Telefone' => $request['tel'],
-            'dataCadastro' => $data,
             'cliente_idCliente'=> $id,
             'CNPJ' => $request['cnpj']
         );
