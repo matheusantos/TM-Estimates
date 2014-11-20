13876ba399adabfc71f28ba434507b33ad0397a6
Matheu <matheusantos@users.noreply.github.com>
2014-11-20 15:06:07 -0300

    mvc

--- application/models/Recuperar.php
+++ application/models/Recuperar.php
@@ -1,39 +1,38 @@
 <?php
 /*! Operações na tabela Recuperar Senha */
-class Application_Model_Recuperar extends Zend_Db_Table_Abstract {
-
-    protected $_name = "recuperarsenha";
-    protected $_primary = "idRecuperar";
+class Application_Model_Recuperar {
 
     public function db_insert($hash, $data_time, $id) {
+        $dao = new Application_Model_DbTable_Recuperarsenha();
         $dados = array(
             'Hash' => $hash,
             'Data_hora' => $data_time,
             'Utilizada' => FALSE,
             'cliente_idCliente' => $id
         );
-        return $this->insert($dados);
+        return $dao->insert($dados);
     }
 
     public function db_select($where = null, $valor = null, $order = null, $limit = null) {
-        $select = $this->select()
-                        ->from($this)
+        $dao = new Application_Model_DbTable_Recuperarsenha();
+        $select = $dao->select()
+                        ->from($dao)
                         ->order($order)
                         ->limit($limit);
 		
         if (!is_null($where)) {
             $select->where($where.'= ?', $valor);
-            //$select->where($where.'='. $valor);
         }
-        return $this->fetchAll($select)->toArray();
+        return $dao->fetchAll($select)->toArray();
     }
 
     public function db_update($hash, $valor) {
+        $dao = new Application_Model_DbTable_Recuperarsenha();
         $dados = array(
             'Utilizada' => $valor
         );
-        $where = $this->getAdapter()->quoteInto("Hash = ?", $hash);
-        $this->update($dados, $where);
+        $where = $dao->getAdapter()->quoteInto("Hash = ?", $hash);
+        $dao->update($dados, $where);
     }
 
 }
