1728eaab7335686673a917528c7c21dbb157ce53
José Barreto dos Santos Neto <jbsneto18@gmail.com>
2014-11-21 21:30:52 -0300

    Gerando relatorios

--- application/controllers/RelatorioController.php
+++ application/controllers/RelatorioController.php
@@ -1,69 +1,97 @@
 <?php
 
-class RelatorioController extends Zend_Controller_Action
-{
+class RelatorioController extends Zend_Controller_Action {
 
-    public function init()
-    {
+    public function init() {
         /* Initialize action controller here */
     }
 
-    public function indexAction()
-    {
+    public function indexAction() {
         // action body
         $this->_helper->layout->setlayout("userlayout");
         $model1 = new Application_Model_Projeto();
         $dados1 = $model1->db_select();
         $this->view->assign("dados1", $dados1);
     }
-    
-    public function relatorioAction()
-    {
+
+    public function relatorioAction() {
         require_once('C:/xampp/htdocs/TM-Estimates/library/fpdf17/fpdf.php');
         // muito importante desabilitar a view de renderizar
         $this->_helper->viewRenderer->setNoRender();
-        
-        $model = new Application_Model_Projeto();
-                
+
+        $model = new Application_Model_ViewRelatorio();
+
         $rows = $model->db_select();
-        
+
         // instancia a classe FPDF
-        $pdf = new FPDF('P','mm','A4');
+        $pdf = new FPDF('P', 'mm', 'A4');
         $pdf->AddPage();
-        $pdf->SetFont('Arial','B',12);
-    
-                    
-        $pdf->Cell(36,10,'Projeto',0,0,'L');
-        $pdf->Cell(36,10,'Est. de custo',0,0, 'L');
-        $pdf->Cell(36,10,'Est. de Esforco',0,0,'L');
-        $pdf->Cell(36,10,'Est. de Prazo',0,0,'L');
-        $pdf->Cell(36,10,'Est. de Produtividade',0,0,'L');
-        //$pdf->Cell(20,10,'Est. de Pontos de função',0,0,'L');
-        
-        // para colocar uma linha em branco
-        $pdf->Cell(109,5,' ',0,0,'C');
-        $pdf->Cell(40,5,' ',0,0,'C');
-        $pdf->Cell(40,5,' ',0,1,'C');
-         $pdf->Cell(109,5,' ',0,0,'C');
-        $pdf->Cell(40,5,' ',0,0,'C');
-        $pdf->Cell(40,5,' ',0,1,'C');
-        
-        $pdf->SetFont('Arial','',12);
-        
+
+
         // percorre os dados 
-        foreach ($rows as $dados)
-        {
-            $pdf->Cell(36,10,$dados['Titulo'],0,0,'L');
-            //$pdf->Cell(60,10,$dados['nascimento'],0,0,'L');
-            //$pdf->Cell(40,10,$dados['id_depto'],0,1,'C');            
+        foreach ($rows as $dados) {
+            $pdf->SetFont('Arial', 'B', 26);
+            $pdf->Cell(190, 20, $dados['Titulo'], 0, 0, 'C');
+            $pdf->Cell(109, 5, ' ', 0, 0, 'C');
+            $pdf->Cell(40, 5, ' ', 0, 0, 'C');
+            $pdf->Cell(40, 5, ' ', 0, 1, 'C');
+            $pdf->Cell(109, 5, ' ', 0, 0, 'C');
+            $pdf->Cell(40, 5, ' ', 0, 0, 'C');
+            $pdf->Cell(40, 5, ' ', 0, 1, 'C');
+            $pdf->Cell(190, 20, 'Relatorio de estimativas', 0, 0, 'C');
+            $pdf->Cell(109, 5, ' ', 0, 0, 'C');
+            $pdf->Cell(40, 5, ' ', 0, 0, 'C');
+            $pdf->Cell(40, 5, ' ', 0, 1, 'C');
+            $pdf->Cell(109, 5, ' ', 0, 0, 'C');
+            $pdf->Cell(40, 5, ' ', 0, 0, 'C');
+            $pdf->Cell(40, 5, ' ', 0, 1, 'C');
+            $pdf->Cell(109, 5, ' ', 0, 0, 'C');
+            $pdf->Cell(40, 5, ' ', 0, 0, 'C');
+            $pdf->Cell(40, 5, ' ', 0, 1, 'C');
+            $pdf->Cell(109, 5, ' ', 0, 0, 'C');
+            $pdf->Cell(40, 5, ' ', 0, 0, 'C');
+            $pdf->Cell(40, 5, ' ', 0, 1, 'C');
+
+            $pdf->SetFont('Arial', 'B', 12);
+
+            $pdf->Cell(36, 10, 'Projeto', 0, 0, 'L');
+            $pdf->Cell(36, 10, 'Est. de custo', 0, 0, 'L');
+            $pdf->Cell(36, 10, 'Est. de Esforco', 0, 0, 'L');
+            $pdf->Cell(36, 10, 'Est. de Prazo', 0, 0, 'L');
+            $pdf->Cell(36, 10, 'Est. de Produtividade', 0, 0, 'L');
+            //$pdf->Cell(20,10,'Est. de Pontos de função',0,0,'L');
+            // para colocar uma linha em branco
+            $pdf->Cell(109, 5, ' ', 0, 0, 'C');
+            $pdf->Cell(40, 5, ' ', 0, 0, 'C');
+            $pdf->Cell(40, 5, ' ', 0, 1, 'C');
+            $pdf->Cell(109, 5, ' ', 0, 0, 'C');
+            $pdf->Cell(40, 5, ' ', 0, 0, 'C');
+            $pdf->Cell(40, 5, ' ', 0, 1, 'C');
+
+            $pdf->SetFont('Arial', '', 12);
+            $pdf->Cell(36, 10, $dados['Titulo'], 0, 0, 'L');
+            $pdf->Cell(36, 10, $dados['Custo'], 0, 0, 'L');
+            $pdf->Cell(36, 10, $dados['Prazo'], 0, 0, 'L');
+            $pdf->Cell(36, 10, $dados['Esforco'], 0, 0, 'L');
+            $pdf->Cell(36, 10, $dados['Produtividade'], 0, 0, 'L');
+
+            $pdf->Cell(109, 5, ' ', 0, 0, 'C');
+            $pdf->Cell(40, 5, ' ', 0, 0, 'C');
+            $pdf->Cell(40, 5, ' ', 0, 1, 'C');
+            $pdf->Cell(109, 5, ' ', 0, 0, 'C');
+            $pdf->Cell(40, 5, ' ', 0, 0, 'C');
+            $pdf->Cell(40, 5, ' ', 0, 1, 'C');
+            $pdf->Cell(109, 5, ' ', 0, 0, 'C');
+            $pdf->Cell(40, 5, ' ', 0, 0, 'C');
+            $pdf->Cell(40, 5, ' ', 0, 1, 'C');
+            $pdf->Cell(109, 5, ' ', 0, 0, 'C');
+            $pdf->Cell(40, 5, ' ', 0, 0, 'C');
+            $pdf->Cell(40, 5, ' ', 0, 1, 'C');
         }
-        
+
         //$pdf->Cell(0,10,'Page '.$pdf->PageNo().'/{nb}',0,0,'C');
-        
         //gera o relatorio
-        $pdf->Output();    
+        $pdf->Output();
     }
 
-
 }
-
--- application/models/ViewRelatorio.php
+++ application/models/ViewRelatorio.php
@@ -3,7 +3,7 @@
 class Application_Model_ViewRelatorio {
 
     public function db_select($where = null, $valor = null, $order = null, $limit = null) {
-        $dao = new Application_Model_DbTable_ViewEstimativasCusto();
+        $dao = new Application_Model_DbTable_ViewRelatorio();
         $select = $dao->select()->from($dao)->order($order)->limit($limit);
         if (!is_null($where)) {
             $select->where($where . '= ?', $valor);
